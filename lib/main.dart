f0df5c969c50cfdd2bb53e8bee404ad735a653f0
Sameer Kamani <155903275+SameerKamani@users.noreply.github.com>
2024-05-18 22:28:26 +0500

    Delete lib directory

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: camel_case_types
-
-import 'package:flutter/material.dart';
-import 'package:flutter_application_2_quiz_app/quiz.dart';
-
-void main() {
-  runApp(const Quiz());
-}
